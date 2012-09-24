39c28160e756e26e5eaf10b390574fd614391d60
jwien001 <jwien001@gmail.com>
2012-09-24 17:57:38 -0400

    Added in graphing library for testing.

--- base_widget/app.js
+++ base_widget/app.js
@@ -11,13 +11,19 @@ $(function() {
             error: ajaxError
 		});
 	});
+	
+	$('busyness_page').bind('pagebeforeshow', function(event, ui) {
+		$.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
+	});
 });
 
 function ajaxError(jqXHR, textStatus, errorThrown) {
-    $('#error_dialog_content').html('<p>An unexpected error occurred<br/>' + textStatus + '<br/>' + errorThrown + '</p>');
+	console.log('ajaxError ' + textStatus + ' ' + errorThrown);
+	// The code below works, but is commented out for testing so that you can still navigate the page if there's an error
+    /*$('#error_dialog_content').html('<p>An unexpected error occurred<br/>' + textStatus + '<br/>' + errorThrown + '</p>');
     $.mobile.changePage($('#error_dialog'), {
             transition: 'pop',
             reverse: false,
             changeHash: false
-    });
+    });*/
 }
