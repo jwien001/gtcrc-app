e23cef3d4a715b241e6b6077d35aff37ff2b5690
jwien001 <jwien001@gmail.com>
2012-09-24 18:02:46 -0400

    Fixing typo in busyness_page beforeload code.

--- base_widget/app.js
+++ base_widget/app.js
@@ -12,7 +12,7 @@ $(function() {
 		});
 	});
 	
-	$('busyness_page').bind('pagebeforeshow', function(event, ui) {
+	$('#busyness_page').bind('pagebeforeshow', function(event, ui) {
 		$.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
 	});
 });
