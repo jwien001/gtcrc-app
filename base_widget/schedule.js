fdc0adb7444a32955024d8d8ad91a5f00c4f0eca
khampton6 <khampton6@gmail.com>
2012-10-04 21:00:37 -0400

    Adding updated schedule code.

--- base_widget/schedule.js
+++ base_widget/schedule.js
@@ -1,6 +1,111 @@
+var gfClasses = [];
+var instClasses = [];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var gfClasses = [new Class("Indoor Cycling", "Monday"),
+
+//Getting JSON from the schedule php API
+$(document).ready(function(){
+    $.getJSON("api/schedule", function(data) {
+    
+    	//For each object in JSON data, parse into a class and add
+    	//to corresponding list.
+        $.each(data, function(){
+            var newClass = new Class(this.ClassName, this.Instructor, 
+                this.StartDate, this.EndDate, 
+                this.DayOfWeek, this.Time, 
+                this.NumOfHours, this.Location, 
+                this.Description, this.Cost, this.ClassType);
+                
+            if(newClass.type == "GITFit")
+            {
+            	window.gfClasses.push(newClass);
+            }
+            else
+            {
+            	window.instClasses.push(newClass);
+            }
+        });
+        
+        //gfClasses.sort(ClassSortFunction);
+        //instClasses.sort(ClassSortFunction);
+        switchToScheduleView();
+    });
+});
+
+//Class List Building Code Section
+
+function buildClassListTable() {
+	
+	var outString = "";
+	
+	return outString;
+}
+
+//Schedule Building Code Section
+
+//Builds string table of GitFit and Inst. Classes.
+function buildScheduleTable() {
+	
+	var outString = '<ul data-role="listview" data-divider-theme="b" data-inset="true">';
+	for(day = 0; day < days.length; day++) {	
+		
+		outString += '<li data-role="list-divider" role="heading">';
+        outString += days[day];
+        outString += '</li>';
+        
+        outString += buildDaySchedule(day+1);
+	}
+	
+	outString += '</ul>';
+	return outString;
+}
+
+//Builds a string list of classes for the day specified.
+function buildDaySchedule(day) {
+
+	var classesOnDay = getClassesForDay(day);
+
+	var outString = "";
+	for(cod = 0; cod < classesOnDay.length; cod++) {
+	
+		outString += '<li data-theme="a"> <a data-rel="dialog" data-transition="slide" onclick="showDialog();" return true;>';
+		outString += classesOnDay[cod].name;
+		outString += '</a> </li>';
+	
+	}
+	return outString;
+}
+
+//Returns an array of classes on the specified day index.
+function getClassesForDay(day) {
+
+	var classesOnDay = [];
+	
+	var gClass = 0;
+	for(gClass = 0; gClass < gfClasses.length; gClass++) {
+		
+		var tClass = gfClasses[gClass];
+		if(tClass.dayOfWeek == day) {
+			classesOnDay.push(tClass);
+			console.log("Day " + day + " " + tClass.name);
+		}
+	}
+	
+	var iClass = 0;
+	for(iClass = 0; iClass < instClasses.length; iClass++) {
+		var tClass = instClasses[iClass];
+		if(tClass.dayOfWeek == day) {
+			classesOnDay.push(tClass);
+			console.log("Day " + day + " " + tClass.name);
+		}
+	}
+	
+	console.log("Return classes size: " + classesOnDay.length);
+	return classesOnDay;
+}
+//End Schedule Code Section
+
+/* = [new Class("Indoor Cycling", "Monday"),
                 new Class("Indoor Cycling", "Friday"), 
                 new Class("Zumba", "Wednesday"),
                 new Class("Urban Dance Grooves", "Tuesday"),
@@ -15,13 +120,17 @@ var instClasses = [new Class("H.E.A.T Boxing", "Sunday"),
                 new Class("Advanced Boxing", "Monday"),
                 new Class("Hatha Yoga", "Thursday"),
                 new Class("Pilates", "Saturday"), 
-                new Class("Vinyasa/ Posture Focus Yoga", "Friday")];
+                new Class("Vinyasa/ Posture Focus Yoga", "Friday")];*/
 
 function injectGITFitClasses() {
+    var out;
+    out='<ul data-role="listview" data-divider-theme="b" data-inset="true">';
     for(i = 0; i < gfClasses.length; i++) {
         var cl = gfClasses[i];
-        document.write('<li data-theme="a"> <a data-transition="slide" onclick=showDialog(); return true;>' + cl.name + '</a> </li>');
+        out+='<li data-theme="a"> <a data-transition="slide" onclick="showDialog()"; return true;>' + cl.name + '</a> </li>';
     }
+    out+= '</ul>';
+    return out;
 }
 
 function injectGITFitClassesForDay(gfDay) {
@@ -66,29 +175,38 @@ function injectClassesForDay(day) {
     injectInstructionalClassesForDay(day);
 }
 
-function Class(name, day) {
+function Class(name, instructor, startDate, endDate, dayOfWeek, time, numOfHours, location, description, cost, type) {
     this.name = name;
-    this.day = day;
+    this.instructor = instructor;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.dayOfWeek = dayOfWeek;
+    this.time = time;
+    this.numOfHours = numOfHours;
+    this.location = location;
+    this.description = description;
+    this.cost = cost;
+    this.type = type;
+}
+
+function ClassSortFunction(a,b) {
+	return (a.name).compareTo(b.name);
 }
 
 //Need to reload tables.
 function switchToScheduleView() {
     element = document.getElementById('ClassTable');
-    element.innerHTML = "<h3>Draw Schedule here.</h3>";
-
-    //Need to redraw the Table.
+    element.innerHTML = buildScheduleTable();
 }
 
 function switchToClassView() {
     element = document.getElementById('ClassTable');
-    element.innerHTML = "<h3>Draw Class List here.</h3>";
-
-    //Need to redraw the Table.
+    element.innerHTML = buildClassListTable();
 }
 
-function showDialog() {
+function showDialog(name) {
     element = document.getElementById('CNameHeader');
-    element.innerHTML = "<h3>Class name here</h3>";
+    element.innerHTML = "<h3>"+name+"</h3>";
     element = document.getElementById('DialogData');
     element.innerHTML = "Class details here";
     $.mobile.changePage("#Dialog", "pop", true, true);
