fb92774de4980156d99769486a8068de936c8afe
Josh <jwien001@gmail.com>
2012-10-06 13:50:46 -0400

    Fixed problems with Upcoming Events widget.

--- base_widget/upcoming_events.js
+++ base_widget/upcoming_events.js
@@ -7,12 +7,28 @@ $(function() {
             dataType: "json",
             async: false,
             success: function(data, textStatus, jqXHR) {
-                    console.log(data);
-	                $("#event_list_row_template").tmpl(data).appendTo("#events_list");
+	            console.log(data);
+	            $("#events_list").html(buildEventList(data));
 	        },
 		    error: ajaxError
 	    });
         
-        $('#comments_list').listview('refresh');
+        $('#events_list').listview('refresh');
 	});	
 });
+
+function buildEventList(data) {
+	var outString = "";
+	
+	for (var i=0; i<data.length; i++) {
+		outString += '<li class="event_list_row">';
+		outString += '<h1>' + data[i].Event_Name + '</h1>';
+		outString += '<span class="ui-li-aside">'
+		outString += new Date(data[i].Date + 'T23:00:00').toDateString();
+		outString += '</span>';
+		outString += '<p>' + data[i].Event_Description + '</p>';
+		outString += '</li>';
+	}
+	
+	return outString;
+}
