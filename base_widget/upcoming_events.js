e8629e180a58bab306c1154d3e9c275e77152b92
Josh <jwien001@gmail.com>
2012-10-06 14:58:53 -0400

    Added Show More button to Events page.

--- base_widget/upcoming_events.js
+++ base_widget/upcoming_events.js
@@ -1,27 +1,37 @@
 $(function() {
 	$('#events_page').bind('pagebeforeshow',function(event, ui) {
-        $(".event_list_row").remove();
-        
-        $.ajax({
-            url: "api/upcoming_events/5",
-            dataType: "json",
-            async: false,
-            success: function(data, textStatus, jqXHR) {
-	            console.log(data);
-	            $("#events_list").html(buildEventList(data));
-	        },
-		    error: ajaxError
-	    });
-        
-        $('#events_list').listview('refresh');
-	});	
+		// Initially load 5 events
+        loadEventList(5);
+	});
+	
+	$('#showMoreEvents').bind('click', function(event, ui) {
+		// Load up to 10 new events with each click
+		var size = $(".eventListRow").length + 10;
+		loadEventList(size);
+	});
 });
+
+function loadEventList(size) {
+	$(".eventListRow").remove();
+    
+    $.ajax({
+        url: "api/upcoming_events/" + size,
+        dataType: "json",
+        async: false,
+        success: function(data, textStatus, jqXHR) {
+            $("#eventsList").html(buildEventList(data));
+        },
+	    error: ajaxError
+    });
+    
+    $('#eventsList').listview('refresh');
+}
 
 function buildEventList(data) {
 	var outString = "";
 	
 	for (var i=0; i<data.length; i++) {
-		outString += '<li class="event_list_row">';
+		outString += '<li class="eventListRow">';
 		outString += '<h1>' + data[i].Event_Name + '</h1>';
 		outString += '<span class="ui-li-aside">'
 		outString += new Date(data[i].Date + 'T23:00:00').toDateString();
