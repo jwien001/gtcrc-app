f73945d9f37d25e6ef14a727f4b608863c3c94a4
Josh <jwien001@gmail.com>
2012-10-07 22:20:24 -0400

    Busyness graph tweaks.

--- base_widget/busyness.js
+++ base_widget/busyness.js
@@ -30,34 +30,46 @@ $(function() {
 	});
 });
 
+var ticks = ['12am', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+             '12pm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'].reverse();
+
 function updateGraph() {
-	//TODO Get data from server
-	var data = [0, 0, 0, 0, 0, 1, 1, 3, 4, 6, 7, 6,
-	             6, 5, 9, dayOfWeek, 9, 6, 5, 7, 8, 6, 4, 3];
-	var ticks = ['12am', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
-	             '12pm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
-	var firstTime = $('#chartdiv').children().length === 0;
-	var chart = $.jqplot('chartdiv',  [data.reverse()], {
-		seriesDefaults: {
-       	renderer: $.jqplot.BarRenderer,
-           rendererOptions: {
-               barDirection: 'horizontal',
-               shadow: false
-           }
-       },
-		axes: {
-			xaxis: {
-				ticks: [[0, 'Empty'], [Math.max.apply(null, data) * 1.2, 'Very Busy']],
-				tickOptions: {showLabel: true}
-			},
-           // Use a category axis on the y axis and use custom ticks
-           yaxis: {
-               renderer: $.jqplot.CategoryAxisRenderer,
-               ticks: ticks.reverse()
-           }
-       }
-	});
-	
-	if (!firstTime)
-		chart.replot({clear: true, resetAxes:true});
+	$.ajax({
+        url: 'api/busyness/' + dayOfWeek,
+		context: document.body,
+        dataType: "json",
+        async: true,
+        success: function(data, textStatus, jqXHR) {
+        	var busyness = [];
+        	for (var i=0; i<data.length; i++) {
+        		busyness[23 - i] = parseInt(data[i]['AVG(count)']);
+        	}
+        	
+        	var firstTime = $('#chartdiv').children().length === 0;
+        	var chart = $.jqplot('chartdiv',  [busyness], {
+        		seriesDefaults: {
+               	renderer: $.jqplot.BarRenderer,
+                   rendererOptions: {
+                       barDirection: 'horizontal',
+                       shadow: false
+                   }
+               },
+        		axes: {
+        			xaxis: {
+        				ticks: [[0, 'Empty'], [Math.max.apply(null, busyness) * 1.05, 'Very Busy']],
+        				tickOptions: {showLabel: true}
+        			},
+                   // Use a category axis on the y axis and use custom ticks
+                   yaxis: {
+                       renderer: $.jqplot.CategoryAxisRenderer,
+                       ticks: ticks
+                   }
+               }
+        	});
+        	
+        	if (!firstTime)
+        		chart.replot({clear: true, resetAxes:true});
+        },
+        error: ajaxError
+	});	
 }
